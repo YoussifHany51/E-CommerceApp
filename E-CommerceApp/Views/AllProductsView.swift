e36c91f1e777ff5c8f17b0ba8f880dac2ba48125
Youssif Hany <youssifhany962@gmail.com>
2024-04-17 01:22:40 +0200

    Detailed View

--- E-CommerceApp/Views/AllProductsView.swift
+++ E-CommerceApp/Views/AllProductsView.swift
@@ -11,54 +11,46 @@ struct AllProductsView: View {
     @ObservedObject var viewModel = ECommerceViewModel()
     @State var viewMoreButton:Bool = true
     @State var isFavButton:Bool = false
-    private let columns = [GridItem(.adaptive(minimum: 170))]
+    var columns = Array(repeating: GridItem(.flexible(),spacing: 20),count:2)
     var body: some View {
         NavigationStack{
             ScrollView{
-                HStack{
-                    Text("ALL ITEMS")
-                        .fontWeight(.heavy)
-                    Spacer()
-                    Button{
-                        viewMoreButton.toggle()
-                    }label: {
-                        Text("VIEW MORE")
-                            .font(.caption2)
-                            .foregroundStyle(.blue)
-                    }
-                }.padding()
+                ViewALLItemSection.padding()
                 LazyVGrid(columns: columns) {
-                    //display products / 8 
-                    ForEach(viewMoreButton ? viewModel.products[0..<viewModel.products.count / 8]:viewModel.products[0..<viewModel.products.count]){ product in
-                        VStack(alignment:.leading) {
+                    //display products / 8
+                        ForEach(viewMoreButton ? viewModel.products[0..<viewModel.products.count / 8]:viewModel.products[0..<viewModel.products.count]){ product in
+                            VStack(alignment:.leading) {
+                                NavigationLink(destination:DetailProductView(title:product.title, description: product.description, image:product.images )) {
                                     HStack{
                                         AsyncImage(url: URL(string: product.images[0])) { image in
-                                                image
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fit)
-                                                    .frame(width: 111, height: 111)
-                                            } placeholder: {
-                                                ProgressView()
-                                            }
+                                            image
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 111, height: 111)
+                                        } placeholder: {
+                                            ProgressView()
                                         }
+                                    }
                                     .padding()
                                     .background(Color.gray.opacity(0.1))
                                     .clipShape(RoundedRectangle(cornerRadius: 10))
                                     .padding(6)
-                            
-                            HStack{
-                                VStack{
-                                    Text(product.title).fontWeight(.heavy)
-                                    Text("\(product.price)").font(.caption2)
                                 }
-                                Spacer()
-                                Button{
-                                    isFavButton.toggle()
-                                }label: {
-                                    Image(systemName: "heart.fill")
-                                        .foregroundStyle(isFavButton ? .red :.gray)
-                                }
-                            }.padding()
+                                
+                                HStack{
+                                    VStack{
+                                        Text(product.title).fontWeight(.heavy)
+                                        Text("\(product.price)").font(.caption2)
+                                    }
+                                    Spacer()
+                                        Button{
+                                            isFavButton.toggle()
+                                        }label: {
+                                            Image(systemName: "heart.fill")
+                                                .foregroundStyle(isFavButton ? .red :.gray)
+                                        }
+                                }.padding()
+                            }
                         }
                     }
                 }
@@ -78,8 +70,25 @@ struct AllProductsView: View {
             }
         }
     }
-}
+
 
 #Preview {
     AllProductsView()
+}
+
+extension AllProductsView{
+   private var ViewALLItemSection:some View{
+        HStack{
+            Text("ALL ITEMS")
+                .fontWeight(.heavy)
+            Spacer()
+            Button{
+                viewMoreButton.toggle()
+            }label: {
+                Text(viewMoreButton ? "VIEW MORE" : "VIEW LESS")
+                    .font(.caption2)
+                    .foregroundStyle(.blue)
+            }
+        }
+    }
 }
---
+++ E-CommerceApp/Views/DetailProductView.swift
@@ -0,0 +1,114 @@
+//
+//  DetailProductView.swift
+//  E-CommerceApp
+//
+//  Created by Youssif Hany on 06/04/2024.
+//
+
+import SwiftUI
+
+struct DetailProductView: View {
+    @State private var isExpanded:Bool =  false
+    @State private var sizeIndex:Int = 3
+    let sizeList = [40,41,42,43,44]
+    let title:String
+    let description:String
+    let image:[String]
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                VStack{
+                    HStack{
+                        AsyncImage(url: URL(string: image[0])) { img in
+                            img
+                                .resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                    }
+                    VStack(alignment: .leading,spacing: 10){
+                        HStack{
+                            Image(systemName: "star.fill")
+                                .foregroundStyle(.yellow)
+                            Text("5.0").fontWeight(.bold)
+                            Text("(42 REVIEWS)")
+                                .kerning(1)
+                        }
+                        .font(.caption)
+                        HStack{
+                            Text(title)
+                                .font(.title3)
+                                .fontWeight(.black)
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundStyle(.blue)
+                        }
+                        Text("DESCRIPTION :")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                        Text(description)
+                            .font(.caption)
+                            .kerning(1)
+                            .lineLimit(isExpanded ? nil : 2)
+                            .overlay (
+                                GeometryReader { proxy in
+                                    Button(action: {
+                                        isExpanded.toggle()
+                                    }) {
+                                        Text(isExpanded ? "less" : "Read more")
+                                            .font(.caption).bold()
+                                            .padding(.leading, 8.0)
+                                            .padding(.top, 4.0)
+                                            .background(Color.white)
+                                            .foregroundStyle(.black)
+                                    }
+                                    .frame(width: proxy.size.width, height: proxy.size.height, alignment: .bottomTrailing)
+                                }
+                            )
+                        Text("SIZE :")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                        
+                        HStack {
+                            ForEach(sizeList,id: \.self){ size in
+                                Text("\(size)")
+                                    .foregroundStyle(sizeIndex == size ? .white : .black)
+                                    .padding()
+                                    .background(sizeIndex == size ? Color.cyan : Color.gray.opacity(0.1))
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .padding(6)
+                                    .font(.caption)
+                                    .onTapGesture {
+                                        sizeIndex = size
+                                    }
+                            }
+                        }
+                        //MARK: Add to cart button
+                        HStack{
+                            Text("")
+                        }
+                        //
+                    }
+                }.padding()
+            }
+            .navigationTitle("ORDER DETAILS")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .toolbar(content: {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button{
+                    
+                }label: {
+                    Image(systemName: "heart.fill")
+                        .foregroundStyle(.red)
+                        .font(.caption)
+                }
+            }
+        })
+    }
+}
+
+//#Preview {
+//    DetailProductView()
+//}
